5ede607c08b25e055a581824d79df172ea0e8486
ahgarawani <ahgarawani@gmail.com>
2022-05-14 18:50:22 +0200

    updating vercel.json

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  //res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
